f7df705162b57f923b520469be0b66f5af6521a2
sartaj <sartaj@sartaj.me>
2020-04-05 11:17:44 -0500

    update

--- src/index.tsx
+++ src/index.tsx
@@ -34,25 +34,50 @@ const App = () => {
           textAlign: "left",
           minHeight: "50vh",
           marginBottom: "2rem",
-          backgroundColor: "#fff",
           padding: "10rem",
         }}
+        className="header-text"
       >
-        <Typography.Title style={{ color: "black", textAlign: "center" }}>
+        <Typography.Title level={2}style={{ textAlign: "center" }}>
           Getting a Better Estimate on Total Coronvirus Cases
         </Typography.Title>
-        <Typography.Paragraph style={{ color: "black", fontSize: "16px" }}>
-          Coronavirus Disease (COVID-19) data around the world has been highly
+        <Typography.Title level={1}>
+          Coronavirus Disease (COVID-19) test data has proven to be highly
           unreliable.
-          <List
-            dataSource={[
-              "In the United States, only cases with noticable symptoms are being tested, and even then, getting tests has been very difficult.",
-            ]}
-            renderItem={(i) => (
-              <List.Item style={{ color: "black" }}>{i}</List.Item>
-            )}
-          />
+        </Typography.Title>
+        <Typography.Paragraph style={{fontSize: '24px'}}>
+          Despite mounting evidence that the majority of positive cases are asymptomatic, countries like the United States only have the capacity to test patients who are showing major symptoms.
+          This means, total cases data, and any data deriving from tit, including mortality rate and infectiousness, are unreliable.
         </Typography.Paragraph>
+        <Typography.Title level={3}>
+          We can do better. Iceland did better.
+        </Typography.Title>
+        <Typography.Paragraph style={{ fontSize: "16px" }}>
+          Since March 14, Iceland, with the assistance of has beeen screening healthy as well as sick people, giving a far more accurate
+          picture of COVID-19. In fact, currently Iceland has the highest tests per million of any major nation.
+        </Typography.Paragraph>
+        <Typography.Paragraph style={{ fontSize: "16px" }}>
+          Their results have been stunning, showing that over half of COVID-19 patients show no symptoms.
+        </Typography.Paragraph>
+        <Typography.Paragraph style={{ fontSize: "16px" }}>
+          What this means is, sure, while the infection rate is much lower, the infenction rate is signficantly higher than any current reported statistics are showing.
+        </Typography.Paragraph>
+        <Typography.Paragraph style={{ fontSize: "16px" }}>
+          What if we used Iceland's data to provide predictions on potential infenction rate in other countries?
+        </Typography.Paragraph>
+        <Typography.Title level={3}>
+          Methodology
+        </Typography.Title>
+        <Typography.Paragraph style={{ fontSize: "16px" }}>
+          The following shows current data of reported total cases in the world, compared to Icelands total cases. The way we do is by looking at 2 statistics that have a high reliabilty rate: the total death count and ICU count. While things like hospitalization rate could be influenced by panic, and infection rate is muddled by the lack of randomized testing, ICU counts and Death counts are done by doctors based on proven medical issues.
+        </Typography.Paragraph>
+        <Typography.Title level={3}>
+          Limitations
+        </Typography.Title>
+        <Typography.Paragraph style={{ fontSize: "16px" }}>
+          The following shows current data of reported total cases in the world, compared to Icelands total cases. The way we do is by looking at 2 statistics that have a high reliabilty rate: the total death count and ICU count. While things like hospitalization rate could be influenced by panic, and infection rate is muddled by the lack of randomized testing, ICU counts and Death counts are done by doctors based on proven medical issues.
+        </Typography.Paragraph>
+
       </div>
       <div className="content">
         <div className="country-card-container">
